3cd577c78b54d1c1ab5e7a2eb00ef3ce3a5f5caa
Ash Kestrel <tetrapteryxgames@gmail.com>
2015-11-16 14:32:32 -0700

    :sparkles: Made the map pretty

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,21 @@
+var mapStyle = $.get('./mapstyles.json');
+var heatMapData = [];
 var map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 0, lng: 0},
-  zoom: 2
+  zoom: 2,
+  mapTypeControl: false,
+  streetViewControl: false,
+  rotateControl: false,
 });
-var heatMapData = [];
 
-$.get('https://paleobiodb.org/data1.2/occs/list.json?base_name=Dinosauria&show=img,coords', function(data) {
+mapStyle.done(function(style){
+  map.setOptions({styles: style});
+});
 
+var dinoData = $.get('https://paleobiodb.org/data1.2/occs/list.json?base_name=Dinosauria&show=img,coords');
+
+dinoData.done(function(data) {
   formatHeatMap(data.records);
-
   var heatmap = new google.maps.visualization.HeatmapLayer({
     data: heatMapData
   });
