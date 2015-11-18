3a19a5fdf38a5e1d6869ec6135c05be00c9edbe1
Ash Kestrel <tetrapteryxgames@gmail.com>
2015-11-17 17:49:19 -0700

    :speech_balloon: Clicking a marker now brings up the dinosaur name

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 var dinoGet = $.get('https://paleobiodb.org/data1.2/occs/list.json?base_name=Dinosauria&min_ma=66&taxon_reso=lump_genus&taxon_status=accepted&show=img,coords');
 var mapStyle = $.get('./mapstyles.json');
+var input = $('#search');
 var heatMapData = [];
 var dinoData;
 var heatMap;
 var markers = [];
+var infoWindows = [];
 var map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 0, lng: 0},
   zoom: 2,
@@ -11,8 +13,10 @@ var map = new google.maps.Map(document.getElementById('map'), {
   streetViewControl: false,
   rotateControl: false,
   minZoom: 2,
-  maxZoom: 10
+  maxZoom: 8
 });
+
+// var searchBox = new google.maps.places.SearchBox(input);
 
 mapStyle.done(function(style){
   map.setOptions({styles: style});
@@ -24,17 +28,12 @@ dinoGet.done(function(data) {
   formatHeatMap(dinoData);
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: heatMapData,
-    // radius: 15,
-    // maxIntensity: 45,
     radius: 3,
     maxIntensity: 25,
     dissipating: false
   });
   for (var i in dinoData){
-    var icon = 'https://paleobiodb.org/data1.2/taxa/icon.png?id=' + dinoData[i].img;
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(dinoData[i].lat, dinoData[i].lng), icon: icon });
-      markers.push(marker);
+    addMarker(dinoData, i);
   }
   updateMarkers();
 });
@@ -66,4 +65,21 @@ function checkBounds() {
           markers[i].setMap(map);
       else
         markers[i].setMap(null);
+}
+
+function addMarker(dinoData, i) {
+  var icon = 'https://paleobiodb.org/data1.2/taxa/icon.png?id=' + dinoData[i].img;
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(dinoData[i].lat, dinoData[i].lng), icon: icon });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      for (var windows in infoWindows)
+        infoWindows[windows].close();
+      infoWindows[i].open(map, marker);
+    });
+
+  markers.push(marker);
+  var info = new google.maps.InfoWindow({
+    content: dinoData[i].tna });
+  infoWindows.push(info);
 }
